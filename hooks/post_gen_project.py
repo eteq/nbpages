b04235af154c525613b4a698a8f49c5150e42e6a
Erik Tollerud <erik.tollerud@gmail.com>
2018-11-05 19:02:32 -0500

    add example-skipping option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,3 +12,8 @@ if '{{ cookiecutter.copy_nbpages }}'.lower() == 'y':
 
     shutil.copytree(nbpages_sourcedir, nbpages_targetdir,
                     ignore=shutil.ignore_patterns('*.pyc', '.*', '__pycache__'))
+
+if '{{ cookiecutter.skip_examples }}'.lower() == 'y':
+    for nbdir in ('{{ cookiecutter.example_notebook }}', '{{ cookiecutter.example_subtopic }}'):
+        to_remove = os.path.join(os.path.realpath(os.path.curdir), nbdir)
+        shutil.rmtree(to_remove)
